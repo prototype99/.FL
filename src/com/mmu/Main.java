3a75c090151d43bb44b6f51d655fc32c67993374
seirra blake <sophietheopossum@yandex.ru>
2022-01-27 02:24:19 +0000

    initialise then quit

--- src/com/mmu/Main.java
+++ src/com/mmu/Main.java
@@ -1,8 +1,12 @@
 package com.mmu;
 
+import static org.libsdl.api.Sdl.SDL_Init;
+import static org.libsdl.api.Sdl.SDL_Quit;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    SDL_Init();
+        SDL_Quit();
     }
 }
