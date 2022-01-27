fcd8e6d975bd35d45958ab98384aa47593ff7e19
seirra blake <sophietheopossum@yandex.ru>
2022-01-27 03:45:02 +0000

    call joystick subsystem

--- src/com/mmu/Main.java
+++ src/com/mmu/Main.java
@@ -1,5 +1,6 @@
 package com.mmu;
 
+import static org.libsdl.api.SDL_SubSystem.SDL_INIT_JOYSTICK;
 import static org.libsdl.api.Sdl.SDL_Init;
 import static org.libsdl.api.Sdl.SDL_Quit;
 import static org.libsdl.api.error.SdlError.SDL_GetError;
@@ -7,7 +8,7 @@ import static org.libsdl.api.error.SdlError.SDL_GetError;
 public class Main {
 
     public static void main(String[] args) {
-        if((SDL_Init() == -1)) {
+        if((SDL_Init(SDL_INIT_JOYSTICK) == -1)) {
             System.out.println(SDL_GetError());
         }
         SDL_Quit();
