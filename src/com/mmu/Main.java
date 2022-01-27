45f77114574e5f0ab2d283cf37bf417416dc4976
seirra blake <sophietheopossum@yandex.ru>
2022-01-27 02:50:32 +0000

    handle errors

--- src/com/mmu/Main.java
+++ src/com/mmu/Main.java
@@ -2,11 +2,14 @@ package com.mmu;
 
 import static org.libsdl.api.Sdl.SDL_Init;
 import static org.libsdl.api.Sdl.SDL_Quit;
+import static org.libsdl.api.error.SdlError.SDL_GetError;
 
 public class Main {
 
     public static void main(String[] args) {
-	    SDL_Init();
+        if((SDL_Init() == -1)) {
+            System.out.println(SDL_GetError());
+        }
         SDL_Quit();
     }
 }
