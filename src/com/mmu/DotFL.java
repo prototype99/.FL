3e6a0a7f8ead57c8548e1d1b7fb1e8efee09f4a6
seirra blake <sophietheopossum@yandex.ru>
2022-05-16 23:03:14 +0100

    remove unnecessary access operation

--- src/com/mmu/DotFL.java
+++ src/com/mmu/DotFL.java
@@ -247,7 +247,7 @@ public class DotFL extends PApplet {
                                 angles[i] = ps[i] - 5 * angles[i];
                             }
                             p.add(angles);
-                            ps = p.get(p.size() - 1);
+                            ps = angles;
                             angle = atan2(ps[1] - mouseY, ps[0] - mouseX);
                             dis = dist(mouseX, mouseY, ps[0], ps[1]);
                         }
