74ac13d87ca04634cf7e28442731d8f16ddab743
seirra blake <sophietheopossum@yandex.ru>
2022-05-16 22:55:56 +0100

    it just works

--- src/com/mmu/DotFL.java
+++ src/com/mmu/DotFL.java
@@ -244,9 +244,9 @@ public class DotFL extends PApplet {
                         while (dis > 5) {
                             float[] angles = new float[]{cos(angle), sin(angle)};
                             for (int i = 0; i < 2; i++) {
-                                ps[i] -= 5 * angles[i];
+                                angles[i] = ps[i] - 5 * angles[i];
                             }
-                            p.add(ps);
+                            p.add(angles);
                             ps = p.get(p.size() - 1);
                             angle = atan2(ps[1] - mouseY, ps[0] - mouseX);
                             dis = dist(mouseX, mouseY, ps[0], ps[1]);
